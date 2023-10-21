54b7fb2d9f15db6d06e2427a3e3697701ea11511
Sergiy Bulba <bulba.sergiy@gmail.com>
2023-10-22 00:03:11 +0300

    Lesson_61_adonet2  07.10.2023

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Market
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_FillGrid = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(538, 272);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Visible = false;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(12, 12);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 41);
+            this.button_Start.TabIndex = 1;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(201, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button_FillGrid
+            // 
+            this.button_FillGrid.Location = new System.Drawing.Point(366, 10);
+            this.button_FillGrid.Name = "button_FillGrid";
+            this.button_FillGrid.Size = new System.Drawing.Size(96, 23);
+            this.button_FillGrid.TabIndex = 3;
+            this.button_FillGrid.Text = "Fill grid";
+            this.button_FillGrid.UseVisualStyleBackColor = true;
+            this.button_FillGrid.Visible = false;
+            this.button_FillGrid.Click += new System.EventHandler(this.button_FillGrid_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose table:";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_FillGrid);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "DB \"Market\"";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button_FillGrid;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Market
+{
+    public partial class Form1 : Form
+    {
+        static string connString = "Data Source=SERG-PC\\SQLEXPRESS;Initial Catalog=Market;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlDataAdapter adapter;
+        DataSet db;
+        SqlConnection connect = new SqlConnection(connString);
+        string commandText = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            button_Start.Visible = false;
+            label1.Visible = true;
+            comboBox1.Visible = true;
+            button_FillGrid.Visible = true;
+            dataGridView1.Visible = true;
+
+            commandText = "select table_name from information_schema.tables";       // запит назв таблиць з бази даних
+
+            adapter = new SqlDataAdapter(commandText, connect);
+            db = new DataSet();
+            adapter.Fill(db);
+            DataTableReader reader = db.CreateDataReader();
+
+            while (reader.Read())                   // заповнення comboBox назвами таблиць
+            {
+                comboBox1.Items.Add((string)reader["TABLE_NAME"]);
+            }
+            reader.Close();
+            connect.Close();
+            GetTablesFromDB();
+           
+        }
+
+        private void GetTablesFromDB()
+        {
+            commandText = @"select* from Sellers; 
+                         select * from Buyers; 
+                         select (Sellers.Name + ' ' + Sellers.Surname) as Seller_name, 
+	                            (Buyers.Name + ' ' + Buyers.Surname) as Buyer_name, 
+	                            Sales.Summ, 
+	                            Sales.Data
+                         from Sales inner join Sellers on Sellers.ID = Sales.SellerID
+		                            inner join Buyers on Buyers.ID = Sales.BuyerID;";
+
+
+            adapter = new SqlDataAdapter(commandText, connect);
+            db = new DataSet();
+
+            var tableNames = comboBox1.Items.Cast<object>().ToArray();      // формування масиву з назвами таблиць БД
+            int i = 0;
+            foreach (var tableName in tableNames)                           // заповнення колекції TableMappings назвами таблиць
+            {
+                if (i == 0)
+                    adapter.TableMappings.Add("Table", tableName.ToString());
+                else
+                    adapter.TableMappings.Add("Table" + i, tableName.ToString());
+                i++;
+            }
+
+            adapter.Fill(db);
+        }
+
+        private void button_FillGrid_Click(object sender, EventArgs e)
+        {
+            // зчитування поточної вибраної назви таблиці з comboBox 
+            string selectedTable = comboBox1.SelectedItem.ToString();
+
+            // заповнення GridView даними з таблиці, що визначається по назві
+            dataGridView1.DataSource = db.Tables[selectedTable];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;        // очищення GridView при виборі нової таблиці
+        }
+    }
+}
